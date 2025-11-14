62c91b1279c43f23e52ade14a39ac97749367949
JCelorioDev <jahir.celorio2001@gmail.com>
2025-11-14 17:10:11 -0500

    feat: :package: install library flowbite in the app

--- src/app/shared/pages/layout/layout.ts
+++ src/app/shared/pages/layout/layout.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Menubar } from "../../components/menubar/menubar";
 
 @Component({
   selector: 'app-layout',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, Menubar],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
